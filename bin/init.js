46058db54f13c72e42991ea9a5e574c6e7af4137
megabyte <megabyte0x@gmail.com>
2025-04-14 23:19:37 +0530

    fix: remove `latest` as the version

--- bin/init.js
+++ bin/init.js
@@ -281,12 +281,12 @@ export async function init() {
 
   // Add dependencies
   packageJson.dependencies = {
-    "@farcaster/auth-client": "latest",
-    "@farcaster/auth-kit": "latest",
-    "@farcaster/frame-core": "latest", 
-    "@farcaster/frame-node": "latest",
-    "@farcaster/frame-sdk": "latest",
-    "@farcaster/frame-wagmi-connector": "latest",
+    "@farcaster/auth-client": ">=0.3.0 <1.0.0",
+    "@farcaster/auth-kit": ">=0.6.0 <1.0.0",
+    "@farcaster/frame-core": ">=0.0.29 <1.0.0",
+    "@farcaster/frame-node": ">=0.0.18 <1.0.0",
+    "@farcaster/frame-sdk": ">=0.0.31 <1.0.0",
+    "@farcaster/frame-wagmi-connector": ">=0.0.19 <1.0.0",
     "@radix-ui/react-label": "^2.1.1",
     "@tanstack/react-query": "^5.61.0",
     "@upstash/redis": "^1.34.3",
