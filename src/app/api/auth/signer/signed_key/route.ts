e0ca42169b4f4970a6652d7a3f5646ed8097dad1
Shreyaschorge <shreyas@neynar.com>
2025-07-11 03:39:27 +0530

    Move constants

--- src/app/api/auth/signer/signed_key/route.ts
+++ src/app/api/auth/signer/signed_key/route.ts
@@ -1,22 +1,12 @@
 import { NextResponse } from 'next/server';
 import { getNeynarClient } from '~/lib/neynar';
 import { mnemonicToAccount } from 'viem/accounts';
+import {
+  SIGNED_KEY_REQUEST_TYPE,
+  SIGNED_KEY_REQUEST_VALIDATOR_EIP_712_DOMAIN,
+} from '~/lib/constants';
 
 const postRequiredFields = ['signerUuid', 'publicKey'];
-
-const SIGNED_KEY_REQUEST_VALIDATOR_EIP_712_DOMAIN = {
-  name: 'Farcaster SignedKeyRequestValidator',
-  version: '1',
-  chainId: 10,
-  verifyingContract:
-    '0x00000000fc700472606ed4fa22623acf62c60553' as `0x${string}`,
-};
-
-const SIGNED_KEY_REQUEST_TYPE = [
-  { name: 'requestFid', type: 'uint256' },
-  { name: 'key', type: 'bytes' },
-  { name: 'deadline', type: 'uint256' },
-];
 
 export async function POST(request: Request) {
   const body = await request.json();
