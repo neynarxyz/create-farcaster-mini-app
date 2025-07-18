9ee370628d7c07d1f7933d6c6cb9e657ce4592a3
Shreyaschorge <shreyas@neynar.com>
2025-07-19 02:53:18 +0530

    if button exists then render it

--- src/components/ui/tabs/ActionsTab.tsx
+++ src/components/ui/tabs/ActionsTab.tsx
@@ -149,7 +149,7 @@ export function ActionsTab() {
       <SignIn />
 
       {/* Neynar Authentication */}
-      <NeynarAuthButton />
+      {NeynarAuthButton && <NeynarAuthButton />}
 
       {/* Mini app actions */}
       <Button
